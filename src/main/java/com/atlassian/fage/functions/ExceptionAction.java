7c6afb30b1f6738e6915611b0fae0dc232f22ae0
Eric Dalgliesh <edalgliesh@atlassian.com>
2010-10-18 23:23:50 +0000

    CR-FAGE-1#c514 Added javadoc to ExceptionAction interface

--- src/main/java/com/atlassian/fage/functions/ExceptionAction.java
+++ src/main/java/com/atlassian/fage/functions/ExceptionAction.java
@@ -1,6 +1,14 @@
 package com.atlassian.fage.functions;
 
+/**
+ * Takes action when an Exception is thrown. Examples include a delay in execution when performing back-offs and
+ * logging errors when exceptions are encountered.
+ * @see ExceptionActions for some predefined handlers
+ */
 public interface ExceptionAction
 {
+    /**
+     * Act on an exception, this method should be called by clients when an exception occurs in wrapped code.
+     */
     void act(Exception exception);
 }
