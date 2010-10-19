66a2d6bdff9df3b4fb12407f6ee01d3d4c4c3e10
Eric Dalgliesh <edalgliesh@atlassian.com>
2010-10-19 07:09:48 +0000

    Added javadoc as per CR-FAGE-1#c513

--- src/main/java/com/atlassian/fage/functions/ExceptionHandlers.java
+++ src/main/java/com/atlassian/fage/functions/ExceptionHandlers.java
@@ -19,10 +19,10 @@ public class ExceptionHandlers
     private ExceptionHandlers() {throw new AssertionError("This class is not instantiable.");}
 
     /**
-     * Retrieves an {ExceptionHandler} which will log exceptions passed in.
+     * Retrieves an {@link ExceptionHandler} which will log exceptions passed in.
      * 
      * @param logger the Logger to which exceptions will be logged; if it is null, a default Logger will be used
-     * @return an {ExceptionHandler} which will log (at WARN level) exceptions passed in
+     * @return an {@link ExceptionHandler} which will log (at WARN level) exceptions passed in
      */
     public static ExceptionHandler loggingExceptionHandler(Logger logger)
     {
@@ -30,14 +30,13 @@ public class ExceptionHandlers
     }
 
     /**
-     * Retrieves an {ExceptionHandler} which will delay for a period whenever it is called. This is useful as a retry
+     * Retrieves an {@link ExceptionHandler} which will delay for a period whenever it is called. This is useful as a retry
      * tool when the failure is due to timing issues, such as a ConcurrentModificationException being thrown or an http
      * request which fails intermittently when temporarily under load. If the delay is interrupted, an exception is
      * logged and normal execution is resumed; in this case the delay may not be as long as specified.
      * 
-     * @param log the Logger to which exceptions will be logged
      * @param delayMilliseconds the desired duration of the delay, in milliseconds
-     * @return an {ExceptionHandler} which will sleep before returning
+     * @return an {@link ExceptionHandler} which will sleep before returning
      */
     public static ExceptionHandler delayingExceptionHandler(int delayMilliseconds)
     {
@@ -45,7 +44,7 @@ public class ExceptionHandlers
     }
 
     /**
-     * @return an {ExceptionHandler} which does nothing
+     * @return an {@link ExceptionHandler} which does nothing
      */
     public static ExceptionHandler noOpExceptionHandler()
     {
