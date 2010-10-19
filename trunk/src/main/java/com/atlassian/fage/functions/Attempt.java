b90d5d5eeafe6b2ced92f41128fe9362217ab351
Eric Dalgliesh <edalgliesh@atlassian.com>
2010-10-19 05:01:44 +0000

    CR-FAGE-1#c558 extracted the implementations of the iteration in Attempt out into decorator classes RetryFunction and RetrySupplier.

--- trunk/src/main/java/com/atlassian/fage/functions/Attempt.java
+++ trunk/src/main/java/com/atlassian/fage/functions/Attempt.java
@@ -2,86 +2,31 @@ package com.atlassian.fage.functions;
 
 import com.google.common.base.Function;
 import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * Provides methods that can be used to attempt to execute a function or supplier a number of times.
+ * Provides factory methods for RetryFunction and RetrySupplier.
  */
 public class Attempt
 {
     private Attempt(){throw new AssertionError("This class is non-instantiable.");}
 
-    /**
-     * Attempt to get the result of supplier <i>tries</i> number of times. Any exceptions thrown by the supplier will be
-     * ignored until the number of attempts is reached. If the number of attempts is reached without a successful result,
-     * the most recent exception to be thrown will be thrown again.
-     * 
-     * @return the first successful result from the supplier
-     */
-    public static <T> T attempt(Supplier<T> supplier, int tries)
+    public static <T> Supplier<T> attempt(Supplier<T> supplier, int tries)
     {
         return attempt(supplier, tries, ExceptionHandlers.noOpExceptionHandler());
     }
     
-    /**
-     * Attempt to get the result of supplier <i>tries</i> number of times. Any exceptions thrown by the supplier will be
-     * acted upon by the exception handler provided until the number of attempts is reached. If the number of attempts is
-     * reached without a successful result, the most recent exception to be thrown will be thrown again.
-     * 
-     * @return the first successful result from the supplier
-     */
-    public static <T> T attempt(Supplier<T> supplier, int tries, ExceptionHandler handler)
+    public static <T> Supplier<T> attempt(Supplier<T> supplier, int tries, ExceptionHandler handler)
     {
-        checkNotNull(supplier);
-        checkNotNull(handler);
-        
-        RuntimeException ex = null;
-        for (int i = 0; i < tries; i++)
-        {
-            try
-            {
-                return supplier.get();
-            }
-            catch (RuntimeException e)
-            {
-                handler.handle(e);
-                ex = e;
-            }
-        }
-        throw ex;
-    }
-    
-    /**
-     * Attempt to get the result of function <i>tries</i> number of times. Any exceptions thrown by the function will be
-     * ignored until the number of attempts is reached. If the number of attempts is reached without a successful result,
-     * the most recent exception to be thrown will be thrown again.
-     * 
-     * @return the first successful result from the function
-     */
-    public static <F, T> T attempt(Function<F, T> function, F parameter, int tries)
-    {
-        return attempt(function, parameter, tries, ExceptionHandlers.noOpExceptionHandler());
-    }
-    
-    /**
-     * Attempt to get the result of function <i>tries</i> number of times. Any exceptions thrown by the function will be
-     * acted upon by the exception handler provided until the number of attempts is reached. If the number of attempts is
-     * reached without a successful result, the most recent exception to be thrown will be thrown again.
-     * 
-     * @return the first successful result from the supplier
-     */
-    public static <F, T> T attempt(Function<F, T> function, F parameter, int tries, ExceptionHandler handler)
-    {
-        checkNotNull(function);
-        checkNotNull(handler);
-        
-        return attempt(toSupplier(function, parameter), tries, handler);
+        return new RetrySupplier<T>(supplier, tries, handler);
     }
 
-    private static <F, T> Supplier<T> toSupplier(final Function<F, T> function, final F parameter)
+    public static <F, T> Function<F, T> attempt(Function<F, T> function, int tries)
     {
-        return Suppliers.compose(function, Suppliers.ofInstance(parameter));
+        return attempt(function, tries, ExceptionHandlers.noOpExceptionHandler());
+    }
+    
+    public static <F, T> Function<F, T> attempt(Function<F, T> function, int tries, ExceptionHandler handler)
+    {
+        return new RetryFunction<F, T>(function, tries, handler);
     }
 }
---
+++ trunk/src/main/java/com/atlassian/fage/functions/RetryFunction.java
@@ -0,0 +1,45 @@
+package com.atlassian.fage.functions;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Suppliers;
+
+/**
+ * A Function which wraps the apply method of another Function and attempts it a fixed number of times. This class can
+ * be used when a task is known to be prone to occasional failure and other workarounds are not known.
+ * 
+ * @param <F> The type of the parameter the Function accepts
+ * @param <T> The type of the result the Function yields upon application
+ * @see RetrySupplier for a Supplier implementation
+ * @see Attempt for some factory methods
+ */
+public class RetryFunction<F, T> implements Function<F, T>
+{
+    private final Function<F, T> function;
+    private final int tries;
+    private final ExceptionHandler handler;
+
+    public RetryFunction(Function<F, T> function, int tries, ExceptionHandler handler)
+    {
+        Preconditions.checkNotNull(function);
+        Preconditions.checkArgument(tries >= 0, "Tries must not be negative");
+        Preconditions.checkNotNull(handler);
+        
+        this.function = function;
+        this.tries = tries;
+        this.handler = handler;
+    }
+
+    /**
+     * Attempt to apply <i>paremeter</i> to the wrapped Function <i>tries</i> number of times. Any exceptions thrown 
+     * will be ignored until the number of attempts is reached. If the number of attempts is reached without a
+     * successful result, the most recent exception to be thrown will be rethrown.
+     * 
+     * @return the result of the wrapped Function's get method
+     */
+    @Override
+    public T apply(F parameter)
+    {
+        return new RetrySupplier<T>(Suppliers.compose(function, Suppliers.ofInstance(parameter)), tries, handler).get();
+    }
+}
