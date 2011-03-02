eab157daff63c4797087d5a2b1e153049b0fc143
Eric Dalgliesh <edalgliesh@atlassian.com>
2011-03-02 00:10:52 +0000

    Removed unused test functions

--- src/test/java/com/atlassian/fage/UtilityFunctions.java
+++ src/test/java/com/atlassian/fage/UtilityFunctions.java
@@ -4,37 +4,6 @@ import com.google.common.base.Function;
 
 public class UtilityFunctions
 {
-    public static class StringToInteger implements Function<Integer, String>
-    {
-        @Override
-        public String apply(Integer integer)
-        {
-            return String.valueOf(integer);
-        }
-    }
-
-    public static class ConcatenateGenerator implements Function<String, Function<String, String>>
-    {
-        @Override
-        public Function<String, String> apply(final String arg1)
-        {
-            return new GeneratedConcatenate(arg1);
-        }
-
-        static class GeneratedConcatenate implements Function<String, String>
-        {
-            private final String arg1;
-
-            public GeneratedConcatenate(final String arg1) {this.arg1 = arg1;}
-
-            @Override
-            public String apply(final String arg2)
-            {
-                return arg1 + arg2;
-            }
-        }
-    }
-    
     public static Function<Integer, Integer> addOne = new Function<Integer, Integer>()
     {
         public Integer apply(Integer integer)
