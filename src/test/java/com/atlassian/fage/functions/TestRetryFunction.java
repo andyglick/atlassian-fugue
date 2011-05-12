4db266e5c4b50bc8764be35a6be13bb00d9b9eb5
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2011-05-12 02:02:04 +0000

    cleanup, removed some unused stuff, source cleanup

--- src/test/java/com/atlassian/fage/functions/TestRetryFunction.java
+++ src/test/java/com/atlassian/fage/functions/TestRetryFunction.java
@@ -1,12 +1,5 @@
 package com.atlassian.fage.functions;
 
-import com.google.common.base.Function;
-import com.google.common.base.Supplier;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertSame;
 import static junit.framework.Assert.fail;
@@ -17,14 +10,22 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.google.common.base.Function;
+
 public class TestRetryFunction
 {
     private static final int ATTEMPTS = 4;
-    
-    @Mock private Supplier<String> supplier;
-    @Mock private Function<String, Integer> function;
-    @Mock private ExceptionHandler exceptionHandler;
-    @Mock private RuntimeException runtimeException;
+
+    @Mock
+    private Function<String, Integer> function;
+    @Mock
+    private ExceptionHandler exceptionHandler;
+    @Mock
+    private RuntimeException runtimeException;
     public static final String INPUT = "1";
     public static final Integer EXPECTED = 1;
 
@@ -33,17 +34,17 @@ public class TestRetryFunction
     {
         initMocks(this);
     }
-    
+
     @Test
     public void testBasicFunction()
     {
         when(function.apply(INPUT)).thenReturn(EXPECTED);
-        Integer result =  new RetryFunction<String, Integer>(function, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).apply(INPUT);
+        final Integer result = new RetryFunction<String, Integer>(function, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).apply(INPUT);
 
         verify(function).apply(INPUT);
         assertEquals(EXPECTED, result);
     }
-    
+
     @Test
     public void testBasicFunctionRetry()
     {
@@ -54,25 +55,25 @@ public class TestRetryFunction
             new RetryFunction<String, Integer>(function, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).apply(INPUT);
             fail("Expected a exception.");
         }
-        catch(RuntimeException e)
+        catch (final RuntimeException e)
         {
             assertSame(runtimeException, e);
         }
-        
+
         verify(function, times(ATTEMPTS)).apply(INPUT);
     }
-    
+
     @Test
     public void testFunctionRetryWithExceptionHandler()
     {
         when(function.apply(INPUT)).thenThrow(runtimeException);
-        
+
         try
         {
             new RetryFunction<String, Integer>(function, ATTEMPTS, exceptionHandler).apply(INPUT);
             fail("Expected a exception.");
         }
-        catch(RuntimeException e)
+        catch (final RuntimeException e)
         {
             assertSame(runtimeException, e);
         }
@@ -80,13 +81,14 @@ public class TestRetryFunction
         verify(function, times(ATTEMPTS)).apply(INPUT);
         verify(exceptionHandler, times(ATTEMPTS)).handle(runtimeException);
     }
-    
+
     @Test
     public void testFunctionEarlyExit()
     {
-        when(function.apply(INPUT)).thenThrow(new RuntimeException("First attempt")).thenReturn(EXPECTED).thenThrow(new RuntimeException("Third attempt")).thenThrow(new RuntimeException("Fourth attempt"));
+        when(function.apply(INPUT)).thenThrow(new RuntimeException("First attempt")).thenReturn(EXPECTED).thenThrow(
+            new RuntimeException("Third attempt")).thenThrow(new RuntimeException("Fourth attempt"));
 
-        Integer result = new RetryFunction<String, Integer>(function, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).apply(INPUT);
+        final Integer result = new RetryFunction<String, Integer>(function, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).apply(INPUT);
         assertEquals(EXPECTED, result);
         verify(function, times(2)).apply(INPUT);
         verifyNoMoreInteractions(function);
--- src/test/java/com/atlassian/fage/functions/TestRetrySupplier.java
+++ src/test/java/com/atlassian/fage/functions/TestRetrySupplier.java
@@ -1,12 +1,5 @@
 package com.atlassian.fage.functions;
 
-import com.google.common.base.Function;
-import com.google.common.base.Supplier;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertSame;
 import static junit.framework.Assert.fail;
@@ -17,72 +10,80 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.google.common.base.Supplier;
+
 public class TestRetrySupplier
 {
     private static final int ATTEMPTS = 4;
     public static final String RESULT = "result";
 
-    @Mock private Supplier<String> supplier;
-    @Mock private Function<String, Integer> function;
-    @Mock private ExceptionHandler exceptionHandler;
-    @Mock private RuntimeException runtimeException;
+    @Mock
+    private Supplier<String> supplier;
+    @Mock
+    private ExceptionHandler exceptionHandler;
+    @Mock
+    private RuntimeException runtimeException;
 
     @Before
     public void setUp()
     {
         initMocks(this);
     }
-    
+
     @Test
     public void testBasicSupplier()
     {
         when(supplier.get()).thenReturn(RESULT);
-        String result = new RetrySupplier<String>(supplier, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).get();
+        final String result = new RetrySupplier<String>(supplier, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).get();
 
         verify(supplier).get();
         assertEquals(RESULT, result);
     }
-    
+
     @Test
     public void testBasicSupplierRetry()
     {
         when(supplier.get()).thenThrow(runtimeException);
-        
+
         try
         {
             new RetrySupplier<String>(supplier, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).get();
             fail("Expected a exception.");
         }
-        catch(RuntimeException e)
+        catch (final RuntimeException e)
         {
             assertSame(runtimeException, e);
         }
-        
+
         verify(supplier, times(ATTEMPTS)).get();
     }
-    
+
     @Test
     public void testSupplierWithExceptionHandler()
     {
         when(supplier.get()).thenReturn(RESULT);
-        String result = new RetrySupplier<String>(supplier, ATTEMPTS, exceptionHandler).get();
-        
+        final String result = new RetrySupplier<String>(supplier, ATTEMPTS, exceptionHandler).get();
+
         verify(supplier).get();
         assertEquals(RESULT, result);
         verifyZeroInteractions(exceptionHandler);
     }
-    
+
     @Test
     public void testSupplierRetryWithExceptions()
     {
         when(supplier.get()).thenThrow(runtimeException);
-        
+
         try
         {
             new RetrySupplier<String>(supplier, ATTEMPTS, exceptionHandler).get();
             fail("Expected a exception.");
         }
-        catch(RuntimeException e)
+        catch (final RuntimeException e)
         {
             assertSame(runtimeException, e);
         }
@@ -90,13 +91,14 @@ public class TestRetrySupplier
         verify(supplier, times(ATTEMPTS)).get();
         verify(exceptionHandler, times(ATTEMPTS)).handle(runtimeException);
     }
-    
-    @Test 
+
+    @Test
     public void testSupplierEarlyExit()
     {
-        when(supplier.get()).thenThrow(new RuntimeException("First attempt")).thenReturn(RESULT).thenThrow(new RuntimeException("Third attempt")).thenThrow(new RuntimeException("Fourth attempt"));
+        when(supplier.get()).thenThrow(new RuntimeException("First attempt")).thenReturn(RESULT).thenThrow(new RuntimeException("Third attempt")).thenThrow(
+            new RuntimeException("Fourth attempt"));
 
-        String result = new RetrySupplier<String>(supplier, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).get();
+        final String result = new RetrySupplier<String>(supplier, ATTEMPTS, ExceptionHandlers.ignoreExceptionHandler()).get();
         assertEquals(RESULT, result);
         verify(supplier, times(2)).get();
         verifyNoMoreInteractions(supplier);
