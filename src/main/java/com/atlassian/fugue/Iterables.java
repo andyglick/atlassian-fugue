5caf7b3e89f5100b7d8617da9f1d06f3625bf2c1
Eric Dalgliesh <edalgliesh@atlassian.com>
2011-08-15 01:08:45 +0000

    CR-FAGE-11: Added javadoc for Iterables. Renamed find to findFirst. Added a test to ensure that Iterables.findFirst does not simply return the first element in the supplied iterable :)

--- src/main/java/com/atlassian/fugue/Iterables.java
+++ src/main/java/com/atlassian/fugue/Iterables.java
@@ -1,17 +1,37 @@
 package com.atlassian.fugue;
 
 import com.google.common.base.Predicate;
-import com.google.common.collect.Iterators;
-import com.google.common.collect.UnmodifiableIterator;
 
+import java.util.Iterator;
+
+import static com.google.common.collect.Iterators.filter;
+
+/**
+ * Contains static utility methods that operate on or return objects of type {code}Iterable{code}.
+ * 
+ * This class is primarily focussed around filling holes in Guava's Iterables class which have become apparent with the
+ * addition of Fugue classes such as Option and Either.
+ * 
+ * When making changes to this class, please try to name methods differently to those in
+ * com.google.common.collect.Iterables so that methods from both classes can be statically imported in the same class.
+ */
 public class Iterables
 {
   private Iterables() { throw new UnsupportedOperationException("This class is not instantiable."); }
-  
-  public static <T> Option<T> find(Iterable<? extends T> ts, Predicate<? super T> p) {
 
-    UnmodifiableIterator<? extends T> t = Iterators.filter(ts.iterator(), p);
-    if(t.hasNext()) {
+  /**
+   * Finds the first item that matches the predicate. 
+   * Traditionally, this should be named find; in this case it ia names findFirst to avoid clashing with static imports
+   * from Guava's Iterables.
+   * 
+   * @param elements the iterable to search for a matching element
+   * @param predicate the predicate to use to determine if an element is eligible to be returned
+   * @return the first item in elements that matches predicate
+   */
+  public static <T> Option<T> findFirst(Iterable<? extends T> elements, Predicate<? super T> predicate) {
+
+    Iterator<? extends T> t = filter(elements.iterator(), predicate);
+    if (t.hasNext()) {
         return Option.some(t.next());
     }
     return Option.none();
--- src/test/java/com/atlassian/fugue/IterablesTest.java
+++ src/test/java/com/atlassian/fugue/IterablesTest.java
@@ -19,35 +19,45 @@ public class IterablesTest {
   };
 
   @Test
-  public void findEmpty() {
+  public void findFirstEmpty() {
     Iterable<Integer> ts = new LinkedList<Integer>();
 
-    Option<Integer> integerOption = Iterables.find(ts, grepOne);
+    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
     assertEquals(Option.<Integer>none(), integerOption);
   }
 
   @Test
-  public void findAbsent() {
+  public void findFirstAbsent() {
 
     List<Integer> ts = new LinkedList<Integer>();
     ts.add(2);
 
-    Option<Integer> integerOption = Iterables.find(ts, grepOne);
+    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
     assertEquals(Option.<Integer>none(), integerOption);
   }
 
   @Test
-  public void findSingle() {
+  public void findFirstSingle() {
 
     List<Integer> ts = new ArrayList<Integer>();
     ts.add(1);
 
-    Option<Integer> integerOption = Iterables.find(ts, grepOne);
+    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
     assertEquals(Option.<Integer>some(1), integerOption);
   }
 
   @Test
-  public void findFirstWhen() {
+  public void findFirstWhenNotFirstElement() {
+    List<Integer> ts = new ArrayList<Integer>();
+    ts.add(2);
+    ts.add(1);
+
+    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
+    assertEquals(Option.<Integer>some(1), integerOption);
+  }
+  
+  @Test
+  public void findFirstMultipleMatches() {
 
     List<Pair<Integer, Integer>> ts = new LinkedList<Pair<Integer, Integer>>();
     Pair<Integer, Integer> expected = pair(1, 1);
@@ -56,7 +66,7 @@ public class IterablesTest {
     ts.add(pair(1,3));
     ts.add(pair(2, 4));
 
-    Option<Pair<Integer, Integer>> found = Iterables.find(ts, new Predicate<Pair<Integer, Integer>>(){
+    Option<Pair<Integer, Integer>> found = Iterables.findFirst(ts, new Predicate<Pair<Integer, Integer>>() {
       public boolean apply(Pair<Integer, Integer> input) {
         return new Integer(1).equals(input.left());
       }
