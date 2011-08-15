94f7dd3ebd6e1f85905f267e6bc9b46f136efb0b
Eric Dalgliesh <edalgliesh@atlassian.com>
2011-08-15 01:18:19 +0000

    CR-FAGE-11: Use Hamcrest matchers and ImmutableList.of in tests.

--- src/test/java/com/atlassian/fugue/IterablesTest.java
+++ src/test/java/com/atlassian/fugue/IterablesTest.java
@@ -1,14 +1,15 @@
 package com.atlassian.fugue;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import static com.atlassian.fugue.Pair.pair;
-import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class IterablesTest {
 
@@ -23,56 +24,43 @@ public class IterablesTest {
     Iterable<Integer> ts = new LinkedList<Integer>();
 
     Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
-    assertEquals(Option.<Integer>none(), integerOption);
+    assertThat(integerOption, is(Option.<Integer>none()));
   }
 
   @Test
   public void findFirstAbsent() {
 
-    List<Integer> ts = new LinkedList<Integer>();
-    ts.add(2);
-
-    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
-    assertEquals(Option.<Integer>none(), integerOption);
+    Option<Integer> integerOption = Iterables.findFirst(ImmutableList.of(2), grepOne);
+    assertThat(integerOption, is(Option.<Integer>none()));
   }
 
   @Test
   public void findFirstSingle() {
 
-    List<Integer> ts = new ArrayList<Integer>();
-    ts.add(1);
-
-    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
-    assertEquals(Option.<Integer>some(1), integerOption);
+    Option<Integer> integerOption = Iterables.findFirst(ImmutableList.of(1), grepOne);
+    assertThat(integerOption, is(Option.<Integer>some(1)));
   }
 
   @Test
   public void findFirstWhenNotFirstElement() {
-    List<Integer> ts = new ArrayList<Integer>();
-    ts.add(2);
-    ts.add(1);
 
-    Option<Integer> integerOption = Iterables.findFirst(ts, grepOne);
-    assertEquals(Option.<Integer>some(1), integerOption);
+    Option<Integer> integerOption = Iterables.findFirst(ImmutableList.of(2,1), grepOne);
+    assertThat(integerOption, is(Option.<Integer>some(1)));
   }
   
   @Test
   public void findFirstMultipleMatches() {
 
-    List<Pair<Integer, Integer>> ts = new LinkedList<Pair<Integer, Integer>>();
     Pair<Integer, Integer> expected = pair(1, 1);
-    ts.add(expected);
-    ts.add(pair(2,2));
-    ts.add(pair(1,3));
-    ts.add(pair(2, 4));
-
+    List<Pair<Integer, Integer>> ts = ImmutableList.of(expected, pair(2, 2), pair(1, 3), pair(2, 4));
+    
     Option<Pair<Integer, Integer>> found = Iterables.findFirst(ts, new Predicate<Pair<Integer, Integer>>() {
       public boolean apply(Pair<Integer, Integer> input) {
         return new Integer(1).equals(input.left());
       }
     });
     
-    assertEquals(Option.some(expected), found);
+    assertThat(found, is(Option.some(expected)));
   }
 
 }
