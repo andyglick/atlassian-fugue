e4a33e2535aa4e2022b797f70e13f6bdce92d3fe
Eric Dalgliesh <edalgliesh@atlassian.com>
2011-08-16 08:14:20 +0000

    Made Functions class package private.

--- src/main/java/com/atlassian/fugue/Functions.java
+++ src/main/java/com/atlassian/fugue/Functions.java
@@ -16,10 +16,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public class Functions {
+/**
+ * This class is package private for the 1.0 release - we intend to make it public once we've decided which methods are
+ * actually useful.
+ */
+ class Functions {
   private Functions() {}
 
-  public static <F, T> T fold(final Function2<T, F, T> f, final T zero, final Iterable<F> elements) {
+   static <F, T> T fold(final Function2<T, F, T> f, final T zero, final Iterable<F> elements) {
     T currentValue = zero;
     for (final F element : elements) {
       currentValue = f.apply(currentValue, element);
@@ -27,9 +31,9 @@ public class Functions {
     return currentValue;
   }
 
-  public static <F, T> T fold(final Function<Pair<T, F>, T> f, final T zero, final Iterable<F> elements) {
+   static <F, T> T fold(final Function<Pair<T, F>, T> f, final T zero, final Iterable<F> elements) {
     return fold(new Function2<T, F, T>() {
-      @Override public T apply(final T arg1, final F arg2) {
+      public T apply(final T arg1, final F arg2) {
         return f.apply(new Pair<T, F>(arg1, arg2));
       }
     }, zero, elements);
@@ -41,12 +45,12 @@ public class Functions {
    * @param <T> the type returned, note the Supplier can be covariant.
    * @return a function that extracts the value from a supplier
    */
-  public static <T> Function<Supplier<? extends T>, T> fromSupplier() {
+   static <T> Function<Supplier<? extends T>, T> fromSupplier() {
     return new ValueExtractor<T>();
   }
 
   private static class ValueExtractor<T> implements Function<Supplier<? extends T>, T> {
-    public T apply(final Supplier<? extends T> supplier) {
+     public T apply(final Supplier<? extends T> supplier) {
       return supplier.get();
     }
   }
@@ -58,12 +62,12 @@ public class Functions {
    * @param <T> the result type
    * @return a Function that transforms an exception into a null
    */
-  public static <T> Function<Supplier<? extends T>, Supplier<T>> ignoreExceptions() {
+   static <T> Function<Supplier<? extends T>, Supplier<T>> ignoreExceptions() {
     return new ExceptionIgnorer<T>();
   }
 
   static class ExceptionIgnorer<T> implements Function<Supplier<? extends T>, Supplier<T>> {
-    public Supplier<T> apply(final Supplier<? extends T> from) {
+     public Supplier<T> apply(final Supplier<? extends T> from) {
       return new IgnoreAndReturnNull<T>(from);
     }
   }
@@ -75,7 +79,7 @@ public class Functions {
       this.delegate = checkNotNull(delegate);
     }
 
-    public T get() {
+     public T get() {
       try {
         return delegate.get();
       } catch (final RuntimeException ignore) {
@@ -84,21 +88,21 @@ public class Functions {
     }
   }
 
-  public static <T> Function<T, List<T>> singletonList(final Class<T> c) {
+   static <T> Function<T, List<T>> singletonList(final Class<T> c) {
     return new SingletonList<T>();
   }
 
   private static final class SingletonList<T> implements Function<T, List<T>> {
-    public List<T> apply(final T o) {
+     public List<T> apply(final T o) {
       return ImmutableList.of(o);
     }
   }
 
-  public static <F, T> Function<F, T> memoize(final Function<F, T> delegate, final MapMaker mapMaker) {
+   static <F, T> Function<F, T> memoize(final Function<F, T> delegate, final MapMaker mapMaker) {
     return new Function<F, T>() {
       final Map<F, T> map = mapMaker.makeComputingMap(delegate);
 
-      public T apply(final F from) {
+      public  T apply(final F from) {
         return map.get(from);
       }
     };
@@ -111,19 +115,19 @@ public class Functions {
       map = mapMaker.makeComputingMap(delegate);
     }
 
-    public T apply(final F from) {
+     public T apply(final F from) {
       return map.get(from);
     }
-  };
+  }
 
-  public static Function<String, Either<NumberFormatException, Long>> parseLong() {
+   static Function<String, Either<NumberFormatException, Long>> parseLong() {
     return ParseLong.INSTANCE;
   }
 
   private enum ParseLong implements Function<String, Either<NumberFormatException, Long>> {
     INSTANCE;
 
-    public Either<NumberFormatException, Long> apply(final String s) {
+     public Either<NumberFormatException, Long> apply(final String s) {
       try {
         return right(Long.valueOf(s));
       } catch (final NumberFormatException e) {
@@ -132,42 +136,42 @@ public class Functions {
     }
   }
 
-  public static <A> Function<A, Iterator<A>> singletonIterator() {
+   static <A> Function<A, Iterator<A>> singletonIterator() {
     return new Function<A, Iterator<A>>() {
-      public Iterator<A> apply(final A a) {
+       public Iterator<A> apply(final A a) {
         return Iterators.singletonIterator(a);
       }
     };
   }
 
-  public static <A, X> Function<X, Iterator<A>> emptyIterator() {
+   static <A, X> Function<X, Iterator<A>> emptyIterator() {
     return new Function<X, Iterator<A>>() {
-      public Iterator<A> apply(final X a) {
+       public Iterator<A> apply(final X a) {
         return ImmutableList.<A> of().iterator();
       }
     };
   }
 
-  public static Function<Object, String> toStringFunction() {
+   static Function<Object, String> toStringFunction() {
     return com.google.common.base.Functions.toStringFunction();
   }
 
-  public static <A> Effect<A> toEffect(final Function<A, ?> function) {
+   static <A> Effect<A> toEffect(final Function<A, ?> function) {
     return new Effect<A>() {
-      public void apply(final A a) {
+       public void apply(final A a) {
         function.apply(a);
       }
     };
   }
 
-  public static Function<String, Either<NumberFormatException, Integer>> parseInt() {
+   static Function<String, Either<NumberFormatException, Integer>> parseInt() {
     return ParseInt.INSTANCE;
   }
 
   private enum ParseInt implements Function<String, Either<NumberFormatException, Integer>> {
     INSTANCE;
 
-    public Either<NumberFormatException, Integer> apply(final String s) {
+     public Either<NumberFormatException, Integer> apply(final String s) {
       try {
         return right(Integer.valueOf(s));
       } catch (final NumberFormatException e) {
@@ -176,14 +180,14 @@ public class Functions {
     }
   }
 
-  public static Function<String, Option<String>> trimToNone() {
+   static Function<String, Option<String>> trimToNone() {
     return TrimToNone.INSTANCE;
   }
 
   private enum TrimToNone implements Function<String, Option<String>> {
     INSTANCE;
 
-    public Option<String> apply(final String s) {
+     public Option<String> apply(final String s) {
       if (s == null) {
         return none();
       }
@@ -192,23 +196,23 @@ public class Functions {
     }
   }
 
-  public static <T> Function<T, T> identity() {
+   static <T> Function<T, T> identity() {
     return com.google.common.base.Functions.identity();
   }
 
-  public static <A> Function<A, Option<A>> option() {
+   static <A> Function<A, Option<A>> option() {
     return new ToOption<A>();
   }
 
   private static class ToOption<A> implements Function<A, Option<A>> {
-    public Option<A> apply(final A from) {
+     public Option<A> apply(final A from) {
       return Option.option(from);
     }
   }
 
   static <A, B> Function<A, B> constant(final B constant) {
     return new Function<A, B>() {
-      public B apply(final A from) {
+       public B apply(final A from) {
         return constant;
       }
     };
