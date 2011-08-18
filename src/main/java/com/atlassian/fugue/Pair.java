97530b81f30f9c8fe8dec6fc1e775f004b0cebff
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2011-08-18 07:08:43 +0000

    FAGE-16 added functions for transforming a Pair into the left or right side values, for instance with Iterables.transform

--- src/main/java/com/atlassian/fugue/Pair.java
+++ src/main/java/com/atlassian/fugue/Pair.java
@@ -2,6 +2,8 @@ package com.atlassian.fugue;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.base.Function;
+
 /**
  * Represents a pair of objects.
  * 
@@ -12,10 +14,39 @@ public final class Pair<A, B> {
 
   /**
    * Factory method for static Pair growth.
+   * 
+   * @param left value, cannot be null
+   * @param right value, cannot be null
    */
   public static <A, B> Pair<A, B> pair(final A left, final B right) {
     return new Pair<A, B>(left, right);
   }
+
+  /**
+   * Function for accessing the left value of {@link Pair pairs}.
+   * 
+   * @param A the left value type
+   * @return a Function that given a {@link Pair} returns the left side value
+   * @since 1.1
+   */
+  public static <A> Function<Pair<A, ?>, A> leftValue() {
+    return new LeftAccessor<A>();
+  }
+
+  /**
+   * Function for accessing the right value of {@link Pair pairs}.
+   * 
+   * @param B the right value type
+   * @return a Function that given a {@link Pair} returns the right side value
+   * @since 1.1
+   */
+  public static <B> Function<Pair<?, B>, B> rightValue() {
+    return new RightAccessor<B>();
+  }
+
+  //
+  // members
+  //
 
   private final A left;
   private final B right;
@@ -60,5 +91,21 @@ public final class Pair<A, B> {
     final int lh = left.hashCode();
     final int rh = right.hashCode();
     return (((lh >> HALF_WORD) ^ lh) << HALF_WORD) | (((rh << HALF_WORD) ^ rh) >> HALF_WORD);
+  }
+
+  //
+  // inner classes
+  //
+
+  static class LeftAccessor<A> implements Function<Pair<A, ?>, A> {
+    @Override public A apply(final Pair<A, ?> from) {
+      return from.left();
+    }
+  }
+
+  static class RightAccessor<B> implements Function<Pair<?, B>, B> {
+    @Override public B apply(final Pair<?, B> from) {
+      return from.right();
+    }
   }
 }
