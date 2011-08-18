8ebf18473690478b2a972d9c7a2b63df31ce8197
Jed Wesley-Smith <jwesleysmith@atlassian.com>
2011-08-18 07:09:05 +0000

    FAGE-16 added functions for transforming a Pair into the left or right side values, for instance with Iterables.transform

--- src/test/java/com/atlassian/fugue/PairTest.java
+++ src/test/java/com/atlassian/fugue/PairTest.java
@@ -1,10 +1,14 @@
 package com.atlassian.fugue;
 
 import static com.atlassian.fugue.Pair.pair;
+import static com.google.common.collect.Iterables.transform;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
 
 public class PairTest {
   @Test(expected = NullPointerException.class) public void testNullLeft() {
@@ -54,5 +58,19 @@ public class PairTest {
 
   @Test public void equalsSameValue() {
     assertThat(pair(1, 3).equals(pair(1, 3)), is(true));
+  }
+
+  @Test public void leftFunction() {
+    final Iterable<Integer> ints = transform(pairs(), Pair.<Integer> leftValue());
+    assertThat(ints, contains(1, 2, 3));
+  }
+
+  @Test public void rightFunction() {
+    final Iterable<String> ints = transform(pairs(), Pair.<String> rightValue());
+    assertThat(ints, contains("1", "2", "3"));
+  }
+
+  private Iterable<Pair<Integer, String>> pairs() {
+    return ImmutableList.of(pair(1, "1"), pair(2, "2"), pair(3, "3"));
   }
 }
