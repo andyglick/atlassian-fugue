fbcf40891dc20028ce3b091fe3a776c9861cbbab
Charles O'Farrell <cofarrell@atlassian.com>
2012-03-23 06:15:44 +0000

    Rename Futures.map in tests too

--- src/test/java/com/atlassian/fugue/FuturesTest.java
+++ src/test/java/com/atlassian/fugue/FuturesTest.java
@@ -6,7 +6,7 @@ import com.google.common.util.concurrent.Futures;
 
 import org.junit.Test;
 
-import static com.atlassian.fugue.Futures.map;
+import static com.atlassian.fugue.Futures.transform;
 import static com.atlassian.fugue.UtilityFunctions.int2String;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -15,11 +15,11 @@ public class FuturesTest {
 
   @Test
   public void testMapGet() throws Exception {
-    assertThat(map(Futures.immediateFuture(1), int2String).get(), is("1"));
+    assertThat(transform(Futures.immediateFuture(1), int2String).get(), is("1"));
   }
 
   @Test
   public void testMapGetTimeout() throws Exception {
-    assertThat(map(Futures.immediateFuture(1), int2String).get(1, TimeUnit.HOURS), is("1"));
+    assertThat(transform(Futures.immediateFuture(1), int2String).get(1, TimeUnit.HOURS), is("1"));
   }
 }
