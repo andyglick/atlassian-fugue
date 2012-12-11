f9dc886c2dbdbaabdfd89c4bba3e2ef17368cedd
Jed Wesley-Smith <jed@atlassian.com>
2012-12-12 08:29:51 +1100

    format according to the local style, removed warnings

--- src/test/java/com/atlassian/fugue/ThrowablesTest.java
+++ src/test/java/com/atlassian/fugue/ThrowablesTest.java
@@ -1,75 +1,82 @@
 package com.atlassian.fugue;
 
-import com.google.common.base.Function;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import com.google.common.base.Function;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class ThrowablesTest {
-    @Mock
-    private Function<Throwable, RuntimeException> function;
+  @Mock
+  private Function<Throwable, RuntimeException> function;
 
-    @Test
-    public void testPropagateWithFunctionForRuntimeException() throws Exception {
-        final Exception original = new RuntimeException();
-        try {
-            Throwables.propagate(original, function);
-            fail("Should have thrown an exception");
-        } catch (Exception e) {
-            assertSame(original, e);
-        }
-
-        verifyZeroInteractions(function);
+  @Test
+  public void testPropagateWithFunctionForRuntimeException() throws Exception {
+    final Exception original = new RuntimeException();
+    try {
+      Throwables.propagate(original, function);
+      fail("Should have thrown an exception");
+    } catch (Exception e) {
+      assertSame(original, e);
     }
 
-    @Test
-    public void testPropagateWithFunctionForNonRuntimeException() throws Exception {
-        final RuntimeException runtime = new RuntimeException();
-        when(function.apply(Mockito.<Throwable>any())).thenReturn(runtime);
+    verifyZeroInteractions(function);
+  }
 
-        final Throwable original = new Exception();
-        try {
-            Throwables.propagate(original, function);
-            fail("Should have thrown an exception");
-        } catch (Exception e) {
-            assertSame(runtime, e);
-        }
+  @Test
+  public void testPropagateWithFunctionForNonRuntimeException() throws Exception {
+    final RuntimeException runtime = new RuntimeException();
+    when(function.apply(Mockito.<Throwable> any())).thenReturn(runtime);
 
-        verify(function).apply(original);
+    final Throwable original = new Exception();
+    try {
+      Throwables.propagate(original, function);
+      fail("Should have thrown an exception");
+    } catch (Exception e) {
+      assertSame(runtime, e);
     }
 
-    @Test
-    public void testPropagateWithTypeForRuntimeException() throws Exception {
-        final Exception original = new RuntimeException();
-        try {
-            Throwables.propagate(original, MyRuntimeException.class);
-            fail("Should have thrown an exception");
-        } catch (Exception e) {
-            assertSame(original, e);
-        }
-    }
+    verify(function).apply(original);
+  }
 
-    @Test
-    public void testPropagateWithTypeForNonRuntimeException() throws Exception {
-        final Exception original = new Exception();
-        try {
-            Throwables.propagate(original, MyRuntimeException.class);
-            fail("Should have thrown an exception");
-        } catch (Exception e) {
-            assertTrue(e instanceof MyRuntimeException);
-            assertSame(original, e.getCause());
-        }
+  @Test
+  public void testPropagateWithTypeForRuntimeException() throws Exception {
+    final Exception original = new RuntimeException();
+    try {
+      Throwables.propagate(original, MyRuntimeException.class);
+      fail("Should have thrown an exception");
+    } catch (Exception e) {
+      assertSame(original, e);
     }
+  }
 
-    private static final class MyRuntimeException extends RuntimeException {
-        public MyRuntimeException(Throwable throwable) {
-            super(throwable);
-        }
+  @Test
+  public void testPropagateWithTypeForNonRuntimeException() throws Exception {
+    final Exception original = new Exception();
+    try {
+      Throwables.propagate(original, MyRuntimeException.class);
+      fail("Should have thrown an exception");
+    } catch (Exception e) {
+      assertTrue(e instanceof MyRuntimeException);
+      assertSame(original, e.getCause());
     }
+  }
+
+  static final class MyRuntimeException extends RuntimeException {
+    private static final long serialVersionUID = 5698445063323657007L;
+
+    public MyRuntimeException(Throwable throwable) {
+      super(throwable);
+    }
+  }
 }
