ebf8f34c3d3a19a1db66d39ee88c14f35e72a2cd
Jed Wesley-Smith <jed@atlassian.com>
2014-05-22 15:32:50 +1000

    added a test for the new right-biased methods

---
+++ fugue/src/test/java/com/atlassian/fugue/EitherRightBiasTest.java
@@ -0,0 +1,30 @@
+package com.atlassian.fugue;
+
+import static com.atlassian.fugue.EitherRightProjectionTest.reverseToEither;
+import static com.atlassian.fugue.UtilityFunctions.addOne;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class EitherRightBiasTest {
+  @Test public void mapRight() {
+      Either<String, Integer> either = Either.right(3);      
+      assertThat(either.map(addOne), is(Either.<String, Integer> right(4)));
+  }
+
+  @Test public void mapLeft() {
+      Either<String, Integer> either = Either.left("foo");      
+      assertThat(either.map(addOne), is(either));
+  }
+
+  @Test public void flatMapRight() {
+      Either<Integer, String> either = Either.right("!foo");      
+      assertThat(either.flatMap(reverseToEither), is(Either.<Integer, String> right("oof!")));
+  }
+
+  @Test public void flatMapLeft() {
+      Either<Integer, String> either = Either.left(5);      
+      assertThat(either.flatMap(reverseToEither), is(either));
+  }
+}
