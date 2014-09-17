52a6a46c8ff2b9932e740dd037931ddf987384f6
Jed Wesley-Smith <jed@atlassian.com>
2014-09-17 11:32:56 +1000

    added @since

--- fugue/src/main/java/com/atlassian/fugue/Unit.java
+++ fugue/src/main/java/com/atlassian/fugue/Unit.java
@@ -3,6 +3,8 @@ package com.atlassian.fugue;
 /**
  * An alternative to {@link Void} that is actually once inhabited (whereas Void
  * is inhabited by null, which causes NPEs).
+ * 
+ * @since 2.2
  */
 public enum Unit {
   VALUE
