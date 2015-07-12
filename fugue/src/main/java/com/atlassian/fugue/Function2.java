869db372fe631a8403ca347af0fdeb28e2d59a81
Anund McKague <amckague@atlassian.com>
2015-07-12 16:32:11 +1000

    Add deprecated Function2 back

---
+++ fugue/src/main/java/com/atlassian/fugue/Function2.java
@@ -0,0 +1,44 @@
+/*
+   Copyright 2011 Atlassian
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.atlassian.fugue;
+
+/**
+ * Represents a function that takes two parameters.
+ *
+ * @param <A> the type of the first argument accepted
+ * @param <B> the type of the result
+ * @param <C> the type of the result of application
+ * @since 1.0
+ * @deprecated since 3.0 use BiFunction instead
+ */
+@Deprecated
+public interface Function2<A, B, C> {
+  /**
+   * Returns the results of applying this function to the supplied arguments.
+   * Like Guava's Function, this method is <em>generally expected</em>, but not
+   * absolutely required, to have the following properties: * it's execution
+   * does not cause any observable side effect * the computation is
+   * <em>consistent with equals</em>; that is, Objects.equal(a, b) implies that
+   * Objects.equal(function.apply(a), function.apply(b)).
+   *
+   * @param arg1 the first argument
+   * @param arg2 the second argument
+   * @return the result, should not be null
+   * @deprecated since 3.0 use BiFunction instead
+   */
+  @Deprecated
+  C apply(A arg1, B arg2);
+}
