e29f492b003273d589be204a13bb70226479057c
Matthew McMahon <mmcmahon@atlassian.com>
2015-08-31 23:53:05 +1000

    Remove tests for deprecated code that fails

--- fugue/src/test/java/com/atlassian/fugue/EitherRightBiasTest.java
+++ fugue/src/test/java/com/atlassian/fugue/EitherRightBiasTest.java
@@ -248,31 +248,4 @@ public class EitherRightBiasTest {
     assertThat(l.ap(func).left().get(), is("woo"));
   }
 
-  @Test
-  public void applyDefinedRight() {
-    final Either<Integer, String> r = right("heyaa!");
-    final Either<Integer, Function<String, String>> func = right(reverse);
-    assertThat(r.apply(func).right().get(), is("!aayeh"));
-  }
-
-  @Test
-  public void applyDefinedLeft() {
-    final Either<Integer, String> r = right("heyaa!");
-    final Either<Integer, Function<String, String>> func = left(36);
-    assertThat(r.apply(func).left().get(), is(36));
-  }
-
-  @Test
-  public void applyNotDefinedRight() {
-    final Either<Integer, String> l = left(12);
-    final Either<Integer, Function<String, String>> func = right(reverse);
-    assertThat(l.apply(func).left().get(), is(12));
-  }
-
-  @Test
-  public void applyNotDefinedLeft() {
-    final Either<Integer, String> l = left(12);
-    final Either<Integer, Function<String, String>> func = left(36);
-    assertThat(l.apply(func).left().get(), is(36));
-  }
 }
