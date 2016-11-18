7450a52676a0077874bb88483db27dbaec312ab0
Gene Taylor <gtaylor@atlassian.com>
2016-11-18 18:46:01 +1100

    ISSUE-57: fix compilation error
    Roll forward changes the try changes,  fixing the compilation error and test failure after the checked refactor.

--- fugue/src/test/java/io/atlassian/fugue/TryTest.java
+++ fugue/src/test/java/io/atlassian/fugue/TryTest.java
@@ -19,13 +19,13 @@ public class TryTest {
 
   private static final Integer STARTING_VALUE = 1;
   private static final String EXCEPTION_MESSAGE = "exception message";
-  private Try<Integer> successT = Try.of(() -> STARTING_VALUE);
-  private Try<Integer> failT = Try.of(() -> {
+  private Try<Integer> successT = Checked.of(() -> STARTING_VALUE);
+  private Try<Integer> failT = Checked.of(() -> {
     throw new IOException(EXCEPTION_MESSAGE);
   });
-  private Function<Integer, Try<String>> f = n -> Try.of(n::toString);
-  private Function<String, Try<String>> g = s -> Try.of(s::toUpperCase);
-  private Function<Integer, Try<Integer>> unit = (Integer x) -> Try.of(() -> x);
+  private Function<Integer, Try<String>> f = n -> Checked.of(n::toString);
+  private Function<String, Try<String>> g = s -> Checked.of(s::toUpperCase);
+  private Function<Integer, Try<Integer>> unit = (Integer x) -> Checked.of(() -> x);
 
   @Test public void leftIdentity() throws Throwable {
     assertThat(unit.apply(STARTING_VALUE).flatMap(f), is(f.apply(STARTING_VALUE)));
@@ -51,13 +51,13 @@ public class TryTest {
   }
 
   @Test public void sequenceReturnsFirstFailure() {
-    Try<String> failed1 = Try.of(() -> {
+    Try<String> failed1 = Checked.of(() -> {
       throw new RuntimeException("FIRST");
     });
-    Try<String> failed2 = Try.of(() -> {
+    Try<String> failed2 = Checked.of(() -> {
       throw new RuntimeException("SECOND");
     });
-    Try<String> failed3 = Try.of(() -> {
+    Try<String> failed3 = Checked.of(() -> {
       throw new RuntimeException("THIRD");
     });
 
@@ -73,7 +73,7 @@ public class TryTest {
   }
 
   @Test public void sequenceReturnsValuesFromAllSuccesses() {
-    Try<Iterable<Integer>> result = Try.sequence(IntStream.range(0, 10).mapToObj(i -> Try.of(() -> i))::iterator);
+    Try<Iterable<Integer>> result = Try.sequence(IntStream.range(0, 10).mapToObj(i -> Checked.of(() -> i))::iterator);
 
     assertThat(result.isSuccess(), is(true));
     Iterable<Integer> vals = result.fold(f -> {
@@ -83,7 +83,7 @@ public class TryTest {
   }
 
   @Test public void flattenNestedSuccess() {
-    Try<Try<Integer>> nested = Try.of(() -> successT);
+    Try<Try<Integer>> nested = Checked.of(() -> successT);
 
     Try<Integer> flattened = Try.flatten(nested);
 
@@ -91,7 +91,7 @@ public class TryTest {
   }
 
   @Test public void flattenNestedFailure() {
-    Try<Try<Integer>> nested = Try.of(() -> failT);
+    Try<Try<Integer>> nested = Checked.of(() -> failT);
 
     Try<Integer> flattened = Try.flatten(nested);
 
