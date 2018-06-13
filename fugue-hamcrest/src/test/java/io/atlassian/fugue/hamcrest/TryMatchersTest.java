08a537582c960b9ebced8908ffeb1e62a3f42203
Antoine Yersin and Julien Fourment <just-dev+ayersin+jfourment@tyro.com>
2018-06-13 15:40:12 +1000

    Split tests for Try matchers into several cases

--- fugue-hamcrest/src/test/java/io/atlassian/fugue/hamcrest/TryMatchersTest.java
+++ fugue-hamcrest/src/test/java/io/atlassian/fugue/hamcrest/TryMatchersTest.java
@@ -11,18 +11,36 @@ import static org.junit.Assert.assertThat;
 
 public class TryMatchersTest {
 
-  @Test public void shouldMatchAFailure() {
+  @Test public void shouldMatchAnyFailure() {
     assertThat(failure(new RuntimeException("any")), TryMatchers.isFailure());
-    assertThat(failure(new RuntimeException("any")), TryMatchers.isFailure(any(Exception.class)));
-    RuntimeException runtimeException = new RuntimeException("any");
-    assertThat(failure(runtimeException), TryMatchers.isFailure(is(runtimeException)));
+  }
+
+  @Test public void shouldNotMatchASuccessWhenExpectingAnyFailure() {
     assertThat(successful("a success"), not(isFailure()));
+  }
+
+  @Test public void shouldMatchASpecificFailure() {
+    assertThat(failure(new RuntimeException("any")), TryMatchers.isFailure(any(Exception.class)));
+  }
+
+  @Test public void shouldNotMatchASuccessWhenExpectingASpecificFailure() {
+    assertThat(successful("a success"), not(isFailure(any(Exception.class))));
+  }
+
+  @Test public void shouldNotMatchADifferentFailure() {
+    assertThat(failure(new IllegalArgumentException("wrong exception")),
+      not(TryMatchers.isFailure(is(new IllegalStateException("expected exception")))));
   }
 
   @Test public void shouldMatchASuccessfulTry() {
     assertThat(successful("anyResult"), isSuccessful(isA(String.class)));
+  }
+
+  @Test public void shouldMatchASuccessfulTryWithASupertypeMatcher() {
     assertThat(successful("anyResult"), isSuccessful(isA(Object.class)));
-    assertThat(successful("anyResult"), isSuccessful(is("anyResult")));
+  }
+
+  @Test public void shouldNotMatchAFailureWhenExpectingASuccess() {
     assertThat(failure(new RuntimeException("any")), not(isSuccessful(any(Object.class))));
   }
 
