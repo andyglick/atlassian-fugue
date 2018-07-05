8e9a820536027fba246d0f0ee68aec586f4e80c8
Matthew McMahon <maccamlc@gmail.com>
2018-07-05 11:37:24 +1000

    Issue #69: Add javadoc for Steps usage

--- fugue-extensions/src/main/java/io/atlassian/fugue/extensions/step/EitherStep1.java
+++ fugue-extensions/src/main/java/io/atlassian/fugue/extensions/step/EitherStep1.java
@@ -7,6 +7,17 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+/**
+ * The first step of the {@link Either} type.
+ *
+ * This class is not intended to be contructed manually, and should only be used as part of a
+ * {@link Steps} chain, started by {@link Steps#begin(Either)}
+ *
+ * @param <A> The right hand side type of the first defined right value
+ * @param <LEFT> The left hand side type of the Either result
+ * @see Steps for usage examples
+ * @since 4.7.0
+ */
 public class EitherStep1<A, LEFT> {
 
   private final Either<LEFT, A> either1;
@@ -15,16 +26,50 @@ public class EitherStep1<A, LEFT> {
     this.either1 = either1;
   }
 
+  /**
+   * Apply the provided function with the previous Step results.
+   *
+   * Internally this will perform a {@link Either#flatMap(Function)} and the result
+   * will become the next step value.
+   *
+   * @param functor The functor to be applied as a flatMap with the previous step
+   * @param <B> The right hand side type of the next step result
+   * @param <LL> The left hand side type of the result that must be related to {@link LEFT}
+   * @return The next step class
+   */
   public <B, LL extends LEFT> EitherStep2<A, B, LEFT> then(Function<? super A, Either<LL, B>> functor) {
     Either<LEFT, B> either2 = either1.flatMap(functor);
     return new EitherStep2<>(either1, either2);
   }
 
+  /**
+   * Apply the provided supplier with the previous Step results.
+   *
+   * Internally this will perform a {@link Either#flatMap(Function)} and the supplier
+   * will become the next step value.
+   *
+   * This is different to {@link #then(Function)} in that the previous step results are
+   * not provided for the new step evaluation.
+   *
+   * @param supplier The supplier to provide the result of the flatMap with the previous step.
+   * @param <B> The right hand side type of the next step result
+   * @param <LL> The left hand side type of the result that must be related to {@link LEFT}
+   * @return The next step class
+   */
   public <B, LL extends LEFT> EitherStep2<A, B, LEFT> then(Supplier<Either<LL, B>> supplier) {
     Either<LEFT, B> either2 = either1.flatMap(value1 -> supplier.get());
     return new EitherStep2<>(either1, either2);
   }
 
+  /**
+   * Apply the provided predicate with the previous step results.
+   *
+   * If the 
+   *
+   * @param predicate
+   * @param unsatisfiedHandler
+   * @return
+   */
   public EitherStep1<A, LEFT> filter(Predicate<? super A> predicate,
     Function<Option<LEFT>, ? extends Either<? extends LEFT, ? extends A>> unsatisfiedHandler) {
     Either<LEFT, A> filterEither1 = either1.filter(predicate, unsatisfiedHandler);
--- fugue-extensions/src/main/java/io/atlassian/fugue/extensions/step/Steps.java
+++ fugue-extensions/src/main/java/io/atlassian/fugue/extensions/step/Steps.java
@@ -6,25 +6,173 @@ import io.atlassian.fugue.Try;
 
 import java.util.Optional;
 
+/**
+ * This provides an entry point for a "for-comprehension" styled syntax shortcut
+ * of fugue {@link Option}, {@link Either}, {@link Try} and Java 8
+ * {@link Optional}.
+ * <p>
+ * The intention of this comprehension is to provide access to flatMap, map and
+ * filter of these <i>monad</i> types in a way that is easier to read and reason
+ * about.
+ * <p>
+ * A short explanation is:
+ *
+ * <ul>
+ * <li>Each step must preserve the shape of the original monad. For example if
+ * you begin with an {@link Option} then each step must return an {@link Option}
+ * with the final yield producing an {@link Option} result.</li>
+ * <li>Each step retains access to the previously defined/success values</li>
+ * <li>On the first empty/failure step result, no further steps will be
+ * evaluated</li>
+ * <li>Filter provides a convenience method to break out of further evaluation</li>
+ * </ul>
+ * <p>
+ * Simple example usage of Steps to produce an {@link Option} by chaining
+ * functions that all have access to the previous values.
+ * <p>
+ * {@code
+ * Option<Integer> result = Steps.begin(some(2))
+ * .then(number -> some(number + 3))
+ * .then((number1, number2) -> some(number1 * number2))
+ * .yield((number1, number2, number3) -> number3 * 4);
+ * }
+ * <p>
+ * This yield will produce a {@link io.atlassian.fugue.Option.Some} containing
+ * the final value of 40.
+ * <p>
+ * This is calculated by:
+ *
+ * <ol>
+ * <li>The initial defined state of the option is 2</li>
+ * <li>This value of 2 is given parameter name of number for the first step, and
+ * is added to 3, producing 5</li>
+ * <li>The next step accepts both parameters (2 and 5) and multiplies them
+ * together, and returns this value of 10</li>
+ * <li>The final yield accepts all 3 parameters (2, 5, 10) but only cares about
+ * the 3rd parameter (10) and multiplies this by 4 to produce 40</li>
+ * <li>The result option is now a <code>some</code> of 40</li>
+ * </ol>
+ * <p>
+ * An example of a Steps causing it to break out early, and do no further
+ * evaluation is:
+ * <p>
+ * {@code
+ * Option<Integer> result = Steps.begin(some(2))
+ * .then(number -> none(Integer.class))
+ * .then((number1, number2) -> some(number1 * number2))
+ * .yield((number1, number2, number3) -> number3 * 4);
+ * }
+ * <p>
+ * In this example, the result will be <code>none</code> and the functions after
+ * the empty state is returned will not be evaluated.
+ * <p>
+ * While each step provides access to the previous values, if these are not
+ * required, then it is possible to evaluate a new value using a Supplier at
+ * each step. For example, the following is possible:
+ * <p>
+ * {@code
+ * Option<Integer> result = Steps.begin(some(2))
+ * .then(() -> some(5))
+ * .then((number1, number2) -> some(number1 * number2))
+ * .yield((number1, number2, number3) -> number3 * 4);
+ * }
+ * <p>
+ * In this case we have decided that the first step will evaluate its result
+ * without requiring the previous state, and just return 5. This final result
+ * will still be a <code>some</code> of 40.
+ * <p>
+ * Filter is another method to break out of further evaulations and return the
+ * empty result. The following example, illustrates this:
+ * <p>
+ * {@code
+ * Option<Integer> result = Steps.begin(some(2))
+ * .then(number -> some(number + 3))
+ * .filter((number1, number2) -> number2 == 0)
+ * .then((number1, number2) -> some(number1 * number2))
+ * .yield((number1, number2, number3) -> number3 * 4);
+ * }
+ * <p>
+ * In this contrived example, the filter predicate will return false as number2
+ * will always be 5, and never 0. This will result in that steps result being
+ * changed to the empty value, and prevent any further step evaluation.
+ * <p>
+ * NB: It is possible to chain together up to 6 steps
+ *
+ * @see Option
+ * @see Either
+ * @see Try
+ * @see Optional
+ * @since 4.7.0
+ */
 public final class Steps {
 
   private Steps() {
     // do not instantiate
   }
 
-  public static <E1, E> EitherStep1<E1, E> begin(Either<E, E1> either) {
+  /**
+   * Begin a new Steps expresison, using the {@link Either} type.
+   * <p>
+   * This assumes a Right-Biased Either, and will therefore continue evaluating
+   * steps on <code>right</code> results, and break out early on the first
+   * <code>left</code> result.
+   *
+   * @param either The start value for this steps.
+   * @param <A> The right hand side type for the initial {@link Either}
+   * @param <LEFT> The left hand side type for the initial {@link Either}
+   * @return A class allowing continuation of this first Step
+   * @see Either
+   * @see Steps for description of Steps usage with examples
+   */
+  public static <A, LEFT> EitherStep1<A, LEFT> begin(Either<LEFT, A> either) {
     return new EitherStep1<>(either);
   }
 
-  public static <E> OptionStep1<E> begin(Option<E> option) {
+  /**
+   * Begin a new Steps expresison, using the {@link Option} type.
+   * <p>
+   * This will continue evaluating steps on <code>some</code> results, and break
+   * out early on the first <code>none</code> result.
+   *
+   * @param option The start value for this steps.
+   * @param <A> The type for the initial {@link Option}
+   * @return A class allowing continuation of this first Step
+   * @see Option
+   * @see Steps for description of Steps usage with examples
+   */
+  public static <A> OptionStep1<A> begin(Option<A> option) {
     return new OptionStep1<>(option);
   }
 
-  @SuppressWarnings("OptionalUsedAsFieldOrParameterType") public static <E> OptionalStep1<E> begin(Optional<E> option) {
-    return new OptionalStep1<>(option);
+  /**
+   * Begin a new Steps expresison, using the {@link Optional} type.
+   * <p>
+   * This will continue evaluating steps on <code>of</code> results, and break
+   * out early on the first <code>empty</code> result.
+   *
+   * @param optional The start value for this steps.
+   * @param <A> The type for the initial {@link Optional}
+   * @return A class allowing continuation of this first Step
+   * @see Optional
+   * @see Steps for description of Steps usage with examples
+   */
+  @SuppressWarnings("OptionalUsedAsFieldOrParameterType") public static <A> OptionalStep1<A> begin(Optional<A> optional) {
+    return new OptionalStep1<>(optional);
   }
 
-  public static <E> TryStep1<E> begin(Try<E> aTry) {
+  /**
+   * Begin a new Steps expresison, using the {@link Try} type.
+   * <p>
+   * This will continue evaluating steps on <code>success</code> results, and
+   * break out early on the first <code>failure</code> result.
+   *
+   * @param aTry The start value for this steps.
+   * @param <A> The type for the initial {@link Try}
+   * @return A class allowing continuation of this first Step
+   * @see Try
+   * @see Steps for description of Steps usage with examples
+   */
+  public static <A> TryStep1<A> begin(Try<A> aTry) {
     return new TryStep1<>(aTry);
   }
 
